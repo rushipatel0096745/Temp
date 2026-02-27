9dfb314b0beea5252b34d0e4f1f727787970b1b1
rushipatel0096745 <rp0096745@gmail.com>
2026-02-27 12:27:49 +0530

    Create tree.tsx

---
+++ tree.tsx
@@ -0,0 +1,21 @@
+app
+ ┣ super-admin
+ ┃ ┣ company
+ ┃ ┃ ┣ add
+ ┃ ┃ ┃ ┗ page.tsx
+ ┃ ┃ ┗ page.tsx
+ ┃ ┣ components
+ ┃ ┃ ┗ Sidebar.tsx
+ ┃ ┣ login
+ ┃ ┃ ┗ page.tsx
+ ┃ ┣ users-and-roles
+ ┃ ┃ ┣ roles
+ ┃ ┃ ┃ ┗ page.tsx
+ ┃ ┃ ┗ users
+ ┃ ┃ ┃ ┗ page.tsx
+ ┃ ┣ layout.tsx
+ ┃ ┗ page.tsx
+ ┣ favicon.ico
+ ┣ globals.css
+ ┣ layout.tsx
+ ┗ page.tsx
